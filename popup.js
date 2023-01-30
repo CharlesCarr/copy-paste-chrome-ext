b7496005888a77308ea433c74692634257e48361
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2023-01-29 23:33:14 -0500

    add local storage for snippets

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
-// Selectors
+// Variables/Selectors
+let snippetsArr;
 const addBtn = document.querySelector(".add-btn");
 const toastCtn = document.querySelector(".toast-ctn");
 const snippetList = document.getElementById("snippet-list");
@@ -46,12 +47,13 @@ const addRow = () => {
   itemDiv.appendChild(copyBtn);
   // Delete Btn
   const deleteBtn = document.createElement("button");
-  // copyBtn.innerHTML = `<i class="fas fa-check"></li>`;
   deleteBtn.innerText = "Delete";
   deleteBtn.classList.add("del-btn");
   itemDiv.appendChild(deleteBtn);
   // Append to list
   snippetList.appendChild(itemDiv);
+  // Add to local storage
+  saveLocalSnippets(inputVal.value);
   // Clear out input field
   document.getElementById("addInput").value = "";
 };
@@ -59,9 +61,17 @@ const addRow = () => {
 const rowBtnHandler = (e) => {
   // console.log(e.target); // <button class="del-btn">Delete</button>
   const item = e.target;
+  console.log(item);
 
   if (item.classList[0] === "del-btn") {
     const parent = item.parentElement;
+    const snipText = parent.firstChild.innerText;
+    console.log("text", snipText);
+    console.log(parent);
+    console.log(parent.dataset.key);
+    // remove from local storage
+    // localStorage.removeItem(`Snip:${parent.dataset.key}`);
+    removeLocalSnippets(parent);
     parent.remove();
   }
 
@@ -71,6 +81,64 @@ const rowBtnHandler = (e) => {
   }
 };
 
+const saveLocalSnippets = (snippet) => {
+  let snippets;
+  if (!localStorage.getItem("snippets")) {
+    snippets = [];
+  } else {
+    snippets = JSON.parse(localStorage.getItem("snippets"));
+    console.log(snippets);
+  }
+  snippets.push(snippet);
+  localStorage.setItem("snippets", JSON.stringify(snippets));
+};
+
+const getLocalSnippets = () => {
+  let snippets;
+  if (!localStorage.getItem("snippets")) {
+    snippets = [];
+  } else {
+    snippets = JSON.parse(localStorage.getItem("snippets"));
+    console.log(snippets);
+  }
+  snippets.forEach((snippet) => {
+    // Create Div
+    const itemDiv = document.createElement("div");
+    itemDiv.classList.add("snippet-item");
+    // Create li
+    const newSnip = document.createElement("li");
+    newSnip.classList.add("snippet-text");
+    newSnip.innerText = snippet;
+    itemDiv.appendChild(newSnip);
+    // Copy Btn
+    const copyBtn = document.createElement("button");
+    copyBtn.innerText = "Copy";
+    copyBtn.classList.add("copy-btn");
+    itemDiv.appendChild(copyBtn);
+    // Delete Btn
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Delete";
+    deleteBtn.classList.add("del-btn");
+    itemDiv.appendChild(deleteBtn);
+    // Append to list
+    snippetList.appendChild(itemDiv);
+  });
+};
+
+const removeLocalSnippets = (snippet) => {
+  let snippets;
+  if (!localStorage.getItem("snippets")) {
+    snippets = [];
+  } else {
+    snippets = JSON.parse(localStorage.getItem("snippets"));
+    console.log(snippets);
+  }
+  const snippetIndex = snippet.children[0].innerText;
+  snippets.splice(snippets.indexOf(snippetIndex), 1);
+  localStorage.setItem("snippets", JSON.stringify(snippets));
+};
+
 // Event Listeners
+document.addEventListener("DOMContentLoaded", getLocalSnippets);
 addBtn.addEventListener("click", addRow);
 snippetList.addEventListener("click", rowBtnHandler);
